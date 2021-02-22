7dd3c377dcba0d36e2db4b9a5d27e511348e4cd5
Kostya Farber <kostya.farber@gmail.com>
2021-02-22 11:50:32 +1100

    Initial Template for callbacks.
    Took 9 hours 12 minutes

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ import dash_bootstrap_components as dbc
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import requests
+import json
 
 
 # Function to import geojson and change to geopandas dataframe
@@ -25,7 +27,6 @@ def remoteGeoJSONToGDF(url, display=False):
 
 # Preparing Data
 # ---------------------------------------------------------------------------------------------------------------------
-
 # import dataframe
 df = pd.read_csv('data/prices_data.csv', parse_dates=True, index_col='Date')
 
@@ -57,46 +58,50 @@ fig = px.choropleth_mapbox(geo_house_prices,
                            center={"lat": -33.865143, "lon": 151.209900},
                            range_color=(0, 2000000),
                            labels={"sellPrice": "Selling Price", "suburb": "Suburb"},
-                           opacity=0.3,
-                           height=1000
+                           opacity=0.6,
+                           height=800,
+                           zoom=8.5
                            )
 
 fig.update_layout(mapbox_style="dark",
-                  mapbox_accesstoken=token,
                   template='plotly_dark',
-                  paper_bgcolor='rgba(0,0,0,0)',
-                  plot_bgcolor='rgba(0,0,0,0)',
-                  title='Median Sydney House Prices',
-                  margin={'r': 0, 'l': 0},
-                  autosize=True
+                  mapbox_accesstoken=token,
+                  autosize=True,
+                  plot_bgcolor='rgba(0, 0, 0, 0)',
+                  paper_bgcolor='rgba(0, 0, 0, 0)',
+                  margin=dict(l=0, r=0, t=0, b=0)
                   )
-fig.update_coloraxes(colorbar_title=dict(side='right', text='Selling Price in Millions (AUD)'))
+fig.update_coloraxes(colorbar_title=dict(side='right', text='Selling Price in Millions (AUD)'),
+                     colorbar=dict(x=0.90, xpad=0))
 fig.update_geos(fitbounds="locations", visible=False)
 
 # Histograms
 # ---------------------------------------------------------------------------------------------------------------------
-bed = px.histogram(data_frame=df, x='bed', template='plotly_dark', color_discrete_sequence=px.colors.sequential.Viridis,
+bed = px.histogram(data_frame=df, x='bed', template='plotly_dark',
                    labels={'bed': 'Number of Bedrooms'}, height=300)
-bed.update_layout(dict(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', title='Bedrooms Histogram'))
+bed.update_layout(dict(title='Bedrooms Histogram', plot_bgcolor='rgba(0, 0, 0, 0)', paper_bgcolor='rgba(0, 0, 0, 0)'))
 bed.update_xaxes(range=[0, 10])
+bed.update_traces(marker=dict(color=px.colors.sequential.Viridis[-4]))
 
 bath = px.histogram(data_frame=df, x='bath', template='plotly_dark',
-                    color_discrete_sequence=px.colors.sequential.Viridis, labels={'bath': 'Number of Bathrooms'},
+                    labels={'bath': 'Number of Bathrooms'},
                     height=300)
-bath.update_layout(dict(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', title='Bathrooms Histogram'))
+bath.update_layout(dict(title='Bathrooms Histogram', plot_bgcolor='rgba(0, 0, 0, 0)', paper_bgcolor='rgba(0, 0, 0, 0)'))
 bath.update_xaxes(range=[0, 10])
+bath.update_traces(marker=dict(color=px.colors.sequential.Viridis[-4]))
 
-car = px.histogram(data_frame=df, x='car', template='plotly_dark', color_discrete_sequence=px.colors.sequential.Viridis,
+car = px.histogram(data_frame=df, x='car', template='plotly_dark',
                    labels={'car': 'Number of Car Spaces'}, height=300)
-car.update_layout(dict(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', title='Car Spaces Histogram'))
+car.update_layout(dict(title='Car Histogram', plot_bgcolor='rgba(0, 0, 0, 0)', paper_bgcolor='rgba(0, 0, 0, 0)'))
 car.update_xaxes(range=[0, 10])
+car.update_traces(marker=dict(color=px.colors.sequential.Viridis[-4]))
 
 sell = px.histogram(data_frame=df, x='sellPrice', template='plotly_dark',
-                    color_discrete_sequence=px.colors.sequential.Viridis,
                     labels={'sellPrice': 'Selling Price in Millions (AUD)'}, height=300)
-sell.update_layout(dict(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', title='Selling Prices Histogram'))
+sell.update_layout(
+    dict(title='Selling Prices Histogram', plot_bgcolor='rgba(0, 0, 0, 0)', paper_bgcolor='rgba(0, 0, 0, 0)'))
 sell.update_xaxes(range=[0, 10000000])
-
+sell.update_traces(marker=dict(color=px.colors.sequential.Viridis[-4]))
 # ---------------------------------------------------------------------------------------------------------------------
 # Count Number of sales made in each month and turn into a dataframe
 sold_per_month = pd.DataFrame(df.index.month_name().value_counts())
@@ -114,46 +119,55 @@ server = app.server
 app.layout = dbc.Container([
 
     dbc.Row([
-        dbc.Col(dcc.Graph(figure=fig)),
+        dbc.Col(children=[
+            html.H4("The Sydney Housing Market from 2000-2019", className='font-weight-light'),
+            html.H5("Explore the median selling prices for Sydney properties and their attributes",
+                    className='font-weight-light')
+        ]),
 
         dbc.Col(children=[
-            dbc.Row([
+            html.Img(src='assets/puzzle.png', width=30, height=30, className='m-1 img-fluid'),
+            html.Img(src='assets/plotly_dash.png', width=128, height=128, className='m-1 img-fluid'),
+        ], style={'textAlign': 'right'})
+    ]),
 
-                dbc.Alert(
-                    (
-                        html.H1('Sydney House Prices'),
-
-                        html.Hr(className='my-2', style={'borderColor': 'white'}, ),
-
-                        html.P(
-                            "Welcome to my dashboard. Sydney Property Prices are always a focal attention in Australian News. "
-                            "Here you have various "
-                            "graphs that can help you better understand the Sydney property market."
-                        ),
-                    )
-                ),
-            ]),
-
-            dbc.Row([
-
-                dbc.Col(dcc.Graph(figure=bed)),
-
-                dbc.Col(dcc.Graph(figure=car))
-
-            ]),
-
-            dbc.Row([
-
-                dbc.Col(dcc.Graph(figure=bath)),
-
-                dbc.Col(dcc.Graph(figure=sell))
-
-            ])
-        ]
+    dbc.Row([
+        dbc.Col(
+            dcc.Graph(figure=fig, id='map', className='bg-dark m-1')
         ),
-    ], no_gutters=False),
+
+        dbc.Col(children=[
+
+            dbc.Row([
+
+                dbc.Col(dcc.Graph(figure=bed), className='m-1 bg-dark'),
+
+                dbc.Col(dcc.Graph(figure=car), className='m-1 bg-dark')
+
+            ]),
+
+            dbc.Row([
+
+                dbc.Col(dcc.Graph(figure=bath), className='m-1 bg-dark'),
+
+                dbc.Col(dcc.Graph(figure=sell), className='m-1 bg-dark'),
+            ]),
+
+            dbc.Row([
+                dbc.Col(html.Pre(id='click-data'), className='m-1 bg-dark')
+            ])
+        ]),
+    ]),
 
 ], fluid=True)
+
+
+@app.callback(
+    Output('click-data', 'children'),
+    Input('map', 'clickData'))
+def display_hover_data(clickData):
+    return json.dumps(clickData, indent=2)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
