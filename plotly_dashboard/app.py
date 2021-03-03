2ee5bd740d53620acc71526d97d7a78b77ddd688
Kostya Farber <kostya.farber@gmail.com>
2021-03-03 19:53:45 +1100

    update app
    Took 17 minutes

--- plotly_dashboard/app.py
+++ plotly_dashboard/app.py
@@ -8,13 +8,16 @@ import plotly.express as px
 import requests
 from dash.dependencies import Input, Output
 import os
+import sys
+
+
 
 # check for tokens
 token = os.getenv('MAPBOX_TOKEN')
 mapbox_style = "dark"
 if not token:
     try:
-        token = open('../.mapbox_token').read()
+        token = open('.mapbox_token').read()
     except Exception as e:
         print('mapbox token not found, using open-street-maps')
         mapbox_style = "carto-darkmatter"
@@ -64,7 +67,7 @@ stat_labels = ['Selling Price is {:.2f}', 'Number of Bedrooms is {:.2f}', 'Numbe
 # Preparing Data
 # ---------------------------------------------------------------------------------------------------------------------
 # import dataframe
-df = pd.read_csv("../data/prices_data.csv", parse_dates=True, index_col='Date')
+df = pd.read_csv("data/prices_data.csv", parse_dates=True, index_col='Date')
 
 sydney = remoteGeoJSONToGDF('https://raw.githubusercontent.com/Perishleaf/data-visualisation-scripts/master'
                             '/dash_project_medium/Sydney_suburb.geojson')
